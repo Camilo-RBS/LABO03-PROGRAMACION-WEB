048be8f54d1a0e9a37db2933768fff05b75f5ead
Camilo-RBS <00083223@uca.edu.sv>
2024-09-03 11:17:28 -0600

    :memo:  se termino el ultimo ejercicio

--- Recursos-Labo-03/Recursos-Labo-03/ejercicio-03/assets/js/main.js
+++ Recursos-Labo-03/Recursos-Labo-03/ejercicio-03/assets/js/main.js
@@ -1,13 +1,29 @@
-const calculateFactorial = (/*recibe*/) => {
-    //Code
+const calculateFactorial = (num) => {
+    if(num<0){
+        return alert("el numero debe de ser un entero positivo")
+    }
+    let factorial = 1;
+    for(let i=1;i<= num; i++){
+        factorial *= i;
+    }
+
+    return factorial;
+ 
+   
 }
 
 const requestNumber = () => {
     //Code
+    let num;
+    num = prompt("Introduce un número entero no negativo para calcular su factorial:");
+    return parseInt(num);
 }
 
 const main = () => {
     //Code
+    let numero = requestNumber();
+    alert("El factorial del número ingresado es: " + calculateFactorial(numero));
+
 }
 
 main();
